61caebad78139ee9b08867f010c98bdfd3d78434
Enda Quigley <endaquigley@gmail.com>
2019-01-10 00:43:22 +0000

    update portfolio

---
+++ public/portfolio/airo/sites/default/files/js/js_PNpUansZIyuykvT1GwITA2yNkoCDOjIwzd5ObOAqFpA.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * Initiate Owl Carousel.
+ */
+
+(function($) {
+  Drupal.behaviors.owlcarousel = {
+    attach: function(context, settings) {
+      for (var carousel in settings.owlcarousel) {
+        // lazyLoad support.
+        if (settings.owlcarousel[carousel].lazyLoad) {
+          var images = $("#" + carousel + ' .field-content').children('img');
+
+          $.each(images, function(i, image) {
+            $(image).attr('data-src', $(image).attr('src'));
+          });
+
+          images.addClass('lazyOwl');
+        }
+
+        // Attach instance settings.
+        $("#" + carousel).owlCarousel(settings.owlcarousel[carousel]);
+      }
+    }
+  };
+}(jQuery));
+;

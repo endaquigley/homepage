0e0678134a71b5fed585025be49a9d34dcad389b
Enda Quigley <endaquigley@gmail.com>
2020-01-06 08:02:53 +0000

    update twitter client

--- twitter.js
+++ twitter.js
@@ -1,29 +1,27 @@
 require('dotenv').config();
 
-const Twitter = require('twitter');
+const Twit = require('twit');
 const to = require('await-to-js').default;
 const tweetParser = require('tweet-parser').default;
 
 let latestTweets = [];
 
-const client = new Twitter({
+const client = new Twit({
   consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECERT,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 (async function fetchTweets() {
 
-  const parameters = {
+  const [ error, response ] = await to(client.get('statuses/user_timeline', {
     count: 5,
     screen_name: 'endaquigley'
-  };
-
-  const [ error, tweets ] = await to(client.get('statuses/user_timeline', parameters));
+  }));
 
   if (error === null) {
-    latestTweets = tweets.map((tweet) => {
+    latestTweets = response.data.map((tweet) => {
       const retweet = tweet.hasOwnProperty('retweeted_status');
       return tweetParser(retweet === true ? tweet.retweeted_status.text : tweet.text);
     });
